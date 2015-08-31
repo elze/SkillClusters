57e33c9973ef6aab2533a40478e1b2b94fd715cf
Cherie Tabb <Cherietabb@gmail.com>
2015-08-31 14:00:13 -0500

    added some angular, switched out links for angular & css, in favor of local files.

---
+++ web/static/js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Cherie Tabb on 8/30/2015.
+ */
+var app = angular.module('skillclusterApp', []);
---
+++ web/static/js/controller.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Cherie Tabb on 8/30/2015.
+ */
+app.controller('MainController', ['$scope', function($scope) {
+    $scope.title = 'Welcome to SkillClusters!';
+    $scope.primary_terms = {
+
+    }
+}]);
+
+
