01b2b72d87dba331ab1ba726a94f52734460d3b4
Cherie Tabb <Cherietabb@gmail.com>
2015-09-12 13:13:09 -0500

    fixed associated terms

--- web/static/js/controller.js
+++ web/static/js/controller.js
@@ -3,14 +3,18 @@
  */
 app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
     $scope.title = 'Welcome to SkillClusters!';
+    $scope.showAssociatedSkills = [];
 
     $http.get('http://127.0.0.1:5000/skills')
         .then(function (result) {
             $scope.skills = result.data;
+            $scope.skills_length = result.data.length;
+            for (var i = 0; i < $scope.skills_length; i++) {
+                $scope.showAssociatedSkills[i] = false;
+            }
 
         });
-    $scope.showAssociatedSkills = [];
-    $scope.setShowAssociatedSkills = function(i) {
+    $scope.setShowAssociatedSkills = function (i) {
         $scope.showAssociatedSkills[i] = true;
     }
 }]);
