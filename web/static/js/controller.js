7023339946b0c9f80632f3feea4c4171a0c2d998
Cherie Tabb <Cherietabb@gmail.com>
2015-09-12 10:23:46 -0500

    switched connection from skills_mock to skills database

--- web/static/js/controller.js
+++ web/static/js/controller.js
@@ -4,12 +4,12 @@
 app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
     $scope.title = 'Welcome to SkillClusters!';
 
-    $http.get('http://127.0.0.1:5000/skills_mock/')
+    $http.get('http://127.0.0.1:5000/skills')
         .then(function (result) {
-            $scope.skills_mock = result.data;
+            $scope.skills = result.data;
 
         });
-}])
-;
-
-
+    $scope.setShowAssociatedSkills = function() {
+        $scope.showAssociatedSkills = true;
+    }
+}]);
