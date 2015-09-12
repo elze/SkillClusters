44a1a00b3c6abb0520b47d9631f4ebde802603c5
Elze Hamilton <elzes@yahoo.com>
2015-09-12 09:32:50 -0500

    Implemented API call that returns list of objects, each of which maps a primary skill to a list of associated skills

--- web/flask_app.py
+++ web/flask_app.py
@@ -1,8 +1,40 @@
 # This also works on Python Anywhere
 
-from flask import Flask
+
+from flask import Flask, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 app = Flask(__name__)
+
+import logging
+from logging import FileHandler
+file_handler = FileHandler("skillClustersLog20150909.txt")
+file_handler.setLevel(logging.WARNING)
+app.logger.addHandler(file_handler)
+
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, datetime):
+        serial = obj.isoformat()
+        return serial
+    return str(obj)
+
+def remove_garbage(obj):
+    serialized = {}
+    for k, v in obj.__dict__.iteritems():
+	if k is not '_sa_instance_state':
+	    serialized[k] = str(v)
+    return serialized
+
+def skill_pair_default(o):
+    if isinstance(o, SkillPair):    
+	return dict(secondary_term=o.secondary_term,
+		number_of_times=o.number_of_times,
+		ratio=str(o.ratio))
+
 
 app.config.from_pyfile('settings.cfg')
 
@@ -17,6 +49,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Float, Numeric
 from sqlalchemy.sql.expression import cast
 
+from datetime import datetime
 import json
 
 import pprint
@@ -42,14 +75,17 @@ class SkillPair(db.Model):
     ratio = db.column_property(
         #db.select([Decimal(number_of_times) / Decimal(SkillPostCounter.number_of_postings)]).\
 	#db.select([SkillPostCounter.number_of_postings / number_of_times]).\
-	db.select([cast(cast(number_of_times, Float) / cast(SkillPostCounter.number_of_postings, Float), Numeric(6, 2))]).\
+	db.select([cast(cast(number_of_times, Float) / cast(SkillPostCounter.number_of_postings, Float), Numeric(7, 3))]).\
             where(SkillPostCounter.skill_term==primary_term).\
             correlate_except(SkillPostCounter)
     )
 
+    def __init__(self, dictionary):
+	for k, v in dictionary.items():
+	    setattr(self, k, v)
+
     def __unicode__(self):
         return self.primary_term
-
 
 
 class SkillView(ModelView):
@@ -69,17 +105,94 @@ class SkillView(ModelView):
         super(SkillView, self).__init__(SkillPair, session, **kwargs)
 
 
+@app.route('/skills')
+def skills():
+
+    engine = db.engine
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    q = session.query(SkillPair)
+    #skillPairs = q.limit(100).all()
+    skillPairs = q.order_by(SkillPair.primary_term.asc(), SkillPair.secondary_term.asc()).all()
+
+    primary_to_secondary = {}
+
+    skills_dictionary = []
+
+    for s in skillPairs:
+	primaryTerm = s.primary_term
+	if primaryTerm not in primary_to_secondary:
+	    primary_to_secondary[primaryTerm] = []
+	    associatedTerms = []
+	    #associatedTerms.append(s)
+	    this_skill_dictionary = dict(primary_term = primaryTerm, associated_terms = associatedTerms)
+	    #skills_dictionary.append(dict(primary_term = primaryTerm, associated_terms = associatedTerms))
+	    skills_dictionary.append(this_skill_dictionary)
+	this_skill_dictionary["associated_terms"].append(s)
+	    
+	#app.logger.debug(primaryTerm)
+	primary_to_secondary[primaryTerm].append(s)
+
+    #app.logger.debug('primary_terms = \n')
+    #app.logger.debug(primary_terms)    
+
+    #skillsJsonString = json.dumps(primary_to_secondary, sort_keys=True, default=skill_pair_default)
+    skillsJsonString = json.dumps(skills_dictionary, sort_keys=True, default=skill_pair_default)
+    return skillsJsonString
+
+
+@app.route('/skills/<int:pageNum>/<int:itemCount>')
+def skillsPage(pageNum, itemCount):
+    # This is not implemented yet: this is the same code as non-paginated skills
+
+    engine = db.engine
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    q = session.query(SkillPair)
+    #skillPairs = q.limit(100).all()
+    skillPairs = q.order_by(SkillPair.primary_term.asc(), SkillPair.secondary_term.asc()).all()
+
+    primary_to_secondary = {}
+
+    skills_dictionary = []
+
+    for s in skillPairs:
+	primaryTerm = s.primary_term
+	if primaryTerm not in primary_to_secondary:
+	    primary_to_secondary[primaryTerm] = []
+	    associatedTerms = []
+	    #associatedTerms.append(s)
+	    this_skill_dictionary = dict(primary_term = primaryTerm, associated_terms = associatedTerms)
+	    #skills_dictionary.append(dict(primary_term = primaryTerm, associated_terms = associatedTerms))
+	    skills_dictionary.append(this_skill_dictionary)
+	this_skill_dictionary["associated_terms"].append(s)
+	    
+	#app.logger.debug(primaryTerm)
+	primary_to_secondary[primaryTerm].append(s)
+
+    #app.logger.debug('primary_terms = \n')
+    #app.logger.debug(primary_terms)    
+
+    #skillsJsonString = json.dumps(primary_to_secondary, sort_keys=True, default=skill_pair_default)
+    skillsJsonString = json.dumps(skills_dictionary, sort_keys=True, default=skill_pair_default)
+    return skillsJsonString
+
+
 @app.route('/')
 def hello_world():
     return '<a href="/admin/">Click me to get to Admin!</a>'
 
 @app.route('/skills_mock/')
 def skills_mock():
-    skillsJsonString = json.dumps([{"primary_term":"Java", "associated_terms": [{"skill_term":"Hibernate","fraction":0.75},{"skill_term":"Oracle","fraction":0.6},{"skill_term":"Spring","fraction":0.5},{"skill_term":"JMS","fraction":0.4},{"skill_term":"JBOSS","fraction":0.7},{"skill_term":"JSP","fraction":0.3},{"skill_term":"HTML","fraction":0.7},{"skill_term":"CSS","fraction":0.6},{"skill_term":"Eclipse","fraction":0.9},{"skill_term":"Maven","fraction":0.8}]},
-				   {"primary_term":"C#", "associated_terms":[{"skill_term":".NET","fraction":0.9},{"skill_term":"NHibernate","fraction":0.4},{"skill_term":"SQL Server","fraction":0.8},{"skill_term":"Entity Framework","fraction":0.7},{"skill_term":"ASP.NET","fraction":0.6},{"skill_term":"NUnit","fraction":0.5},{"skill_term":"Team Foundation Server","fraction":0.4},{"skill_term":"HTML","fraction":0.7},{"skill_term":"CSS","fraction":0.6},{"skill_term":"Visual Studio","fraction":0.9},{"skill_term":"Javascript","fraction":0.6},{"skill_term":"Powershell","fraction":0.1}]},
-				   {"primary_term":"Python", "associated_terms":[{"skill_term":"Django","fraction":0.7},{"skill_term":"Flask","fraction":0.4},{"skill_term":"Bottle","fraction":0.1},{"skill_term":"SQLAlchemy","fraction":0.2},{"skill_term":"GIS","fraction":0.5},{"skill_term":"Postgres","fraction":0.4},{"skill_term":"Git","fraction":0.6},{"skill_term":"HTML","fraction":0.4},{"skill_term":"CSS","fraction":0.3},{"skill_term":"Javascript","fraction":0.4}]},
-				   {"primary_term":"Ruby", "associated_terms":[{"skill_term":"Ruby on Rails","fraction":0.8},{"skill_term":"Sinatra","fraction":0.3},{"skill_term":"Elixir","fraction":0.1},{"skill_term":"ActiveRecord","fraction":0.2},{"skill_term":"Postgres","fraction":0.2},{"skill_term":"Git","fraction":0.6},{"skill_term":"HTML","fraction":0.6},{"skill_term":"CSS","fraction":0.6},{"skill_term":"Javascript","fraction":0.5},{"skill_term":"Mocha","fraction":0.1},{"skill_term":"Cucumber","fraction":0.4}]},
-				   {"primary_term":"Javascript", "associated_terms":[{"skill_term":"Angular","fraction":0.6},{"skill_term":"React","fraction":0.4},{"skill_term":"Backbone","fraction":0.5},{"skill_term":"Knockout","fraction":0.3},{"skill_term":"Node","fraction":0.6},{"skill_term":"Express","fraction":0.2},{"skill_term":"Git","fraction":0.9},{"skill_term":"HTML","fraction":0.9},{"skill_term":"CSS","fraction":0.9},{"skill_term":"Mocha","fraction":0.1},{"skill_term":"Chai","fraction":0.1}]}
+    skillsJsonString = json.dumps([
+	{"primary_term":"C#", "associated_terms":[{"skill_term":".NET","fraction":0.9},{"skill_term":"NHibernate","fraction":0.4},{"skill_term":"SQL Server","fraction":0.8},{"skill_term":"Entity Framework","fraction":0.7},{"skill_term":"ASP.NET","fraction":0.6},{"skill_term":"NUnit","fraction":0.5},{"skill_term":"Team Foundation Server","fraction":0.4},{"skill_term":"HTML","fraction":0.7},{"skill_term":"CSS","fraction":0.6},{"skill_term":"Visual Studio","fraction":0.9},{"skill_term":"Javascript","fraction":0.6},{"skill_term":"Powershell","fraction":0.1}]},
+	{"primary_term":"Java", "associated_terms": [{"skill_term":"Hibernate","fraction":0.75},{"skill_term":"Oracle","fraction":0.6},{"skill_term":"Spring","fraction":0.5},{"skill_term":"JMS","fraction":0.4},{"skill_term":"JBOSS","fraction":0.7},{"skill_term":"JSP","fraction":0.3},{"skill_term":"HTML","fraction":0.7},{"skill_term":"CSS","fraction":0.6},{"skill_term":"Eclipse","fraction":0.9},{"skill_term":"Maven","fraction":0.8}]},
+	{"primary_term":"Javascript", "associated_terms":[{"skill_term":"Angular","fraction":0.6},{"skill_term":"React","fraction":0.4},{"skill_term":"Backbone","fraction":0.5},{"skill_term":"Knockout","fraction":0.3},{"skill_term":"Node","fraction":0.6},{"skill_term":"Express","fraction":0.2},{"skill_term":"Git","fraction":0.9},{"skill_term":"HTML","fraction":0.9},{"skill_term":"CSS","fraction":0.9},{"skill_term":"Mocha","fraction":0.1},{"skill_term":"Chai","fraction":0.1}]},
+	{"primary_term":"PHP", "associated_terms":[{"skill_term":"Angular","fraction":0.3},{"skill_term":"Ember","fraction":0.6},{"skill_term":"Git","fraction":0.3},{"skill_term":"LAMP","fraction":0.9},{"skill_term":"Linux","fraction":0.6},{"skill_term":"MVC","fraction":0.6},{"skill_term":"Oracle","fraction":0.4},{"skill_term":"REST","fraction":0.7},{"skill_term":"MySQL","fraction":0.8},{"skill_term":"HTML","fraction":0.4},{"skill_term":"CSS","fraction":0.3},{"skill_term":"Javascript","fraction":0.4},{"skill_term":"Symfony","fraction":0.4},{"skill_term":"Zend","fraction":0.2}]},
+	{"primary_term":"Python", "associated_terms":[{"skill_term":"Django","fraction":0.7},{"skill_term":"Flask","fraction":0.4},{"skill_term":"Bottle","fraction":0.1},{"skill_term":"SQLAlchemy","fraction":0.2},{"skill_term":"GIS","fraction":0.5},{"skill_term":"Postgres","fraction":0.4},{"skill_term":"Git","fraction":0.6},{"skill_term":"HTML","fraction":0.4},{"skill_term":"CSS","fraction":0.3},{"skill_term":"Javascript","fraction":0.4}]},
+	{"primary_term":"Ruby", "associated_terms":[{"skill_term":"Ruby on Rails","fraction":0.8},{"skill_term":"Sinatra","fraction":0.3},{"skill_term":"Elixir","fraction":0.1},{"skill_term":"ActiveRecord","fraction":0.2},{"skill_term":"Postgres","fraction":0.2},{"skill_term":"Git","fraction":0.6},{"skill_term":"HTML","fraction":0.6},{"skill_term":"CSS","fraction":0.6},{"skill_term":"Javascript","fraction":0.5},{"skill_term":"Mocha","fraction":0.1},{"skill_term":"Cucumber","fraction":0.4}]}
 				   ],
 				  indent=4
 				   )
