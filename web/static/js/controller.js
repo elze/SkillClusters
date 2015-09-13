1ae6227f5482bb0b3a704def251e6f5de9b93cfe
Elze Hamilton <elzes@yahoo.com>
2015-09-12 20:51:54 -0500

    Made the secondary skill boxes a little more reasonable looking

--- web/static/js/controller.js
+++ web/static/js/controller.js
@@ -5,6 +5,9 @@ app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
     $scope.title = 'Welcome to SkillClusters!';
     $scope.showAssociatedSkills = [];
     $scope.secondarySkillStyle = [];
+    $scope.ratiosToDisplay = [];
+
+    secondaryBoxColors = ['#D6FFEB', '#C2FFE0', '#ADFFD6', '#99FFCC', '#7ACCA3', '#5C997A', '#3D6652'];
 
     $http.get('http://127.0.0.1:5000/skills')
         .then(function (result) {
@@ -14,20 +17,74 @@ app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
                 $scope.showAssociatedSkills[i] = false;
                 var skill = $scope.skills[i];
                 $scope.secondarySkillStyle[i] = [];
+		$scope.ratiosToDisplay[i] = [];
                 for (var j = 0; j < skill.associated_terms.length; j++) {
 		  var width = skill.associated_terms[j].ratio * 7000;
-		  var remainderWidth = width;
+		  var sqRootWidth = Math.floor(Math.sqrt(width));
+		  var computedSide = width;
+		  if (sqRootWidth > 100) {
+		    computedSide = sqRootWidth;
+		  }
+		  //var remainderWidth = width;
+		  var remainderWidth = computedSide;
 		  var previousRemainderWidth = remainderWidth;
 		  var height = 30;
-		  while (remainderWidth > 500) {
-		    previousRemainderWidth = remainderWidth;
-		    remainderWidth = remainderWidth - 500;
-		    height = height + 30;
+		  var padding = 0;
+		  var fontSize = 100;
+		  var fontSizeFactor = 1;
+		  var paddingIncrease = 0;
+		  var padding = 0;
+		  var wordBreak = 'normal';
+		  var boxColor = '#D6FFEB';
+		  var ratioToDisplay;
+		  if (skill.associated_terms[j].ratio < 0.01) {
+		    //ratioToDisplay = "< 0.01";
+		    $scope.ratiosToDisplay[i][j] = "< 0.01";
 		  }
+		  else {
+		    //ratioToDisplay = skill.associated_terms[j].ratio.toFixed(2);
+		    var ratioFloat = Number.parseFloat(skill.associated_terms[j].ratio);
+		    //$scope.ratiosToDisplay[i][j] = skill.associated_terms[j].ratio.toFixed(2);
+		    $scope.ratiosToDisplay[i][j] = ratioFloat.toFixed(2);
+		  }
+
+		  var fontScaleFactor = 1;
+		  /*********
+		  while (remainderWidth > 300) {
+		    previousRemainderWidth = remainderWidth;
+		    remainderWidth = remainderWidth - 300;
+		    height = height + 1;
+		    paddingIncrease = paddingIncrease + 2;
+		    //padding = "30% 0%";		    
+		    fontSizeFactor = fontSizeFactor + 1;
+		  }
+		  **********/
+		  if (computedSide > 200) {
+		    var boxSizeFactor = computedSide / 200;
+		    var colorIndex = Math.floor(boxSizeFactor / 2);
+		    if (colorIndex > secondaryBoxColors.length - 1) {
+		      //console.log("colorIndex = " + colorIndex + " larger than secondaryBoxColors.length - 1");
+		      colorIndex = secondaryBoxColors.length - 1;
+		    }
+		    boxColor = secondaryBoxColors[colorIndex];
+		    var boxScaleFactor = 0.02 * boxSizeFactor;
+		    var fontScaleFactor = 1 + 0.1 * boxSizeFactor;
+		    computedSide = Math.floor(200 + 100 * boxScaleFactor);
+		    paddingIncrease = Math.floor(boxSizeFactor * 1.05);
+		    wordBreak = 'break-all';
+		  }
+
 		  previousRemainderWidth = previousRemainderWidth + "px";
 		  height = height + "px";
+		  fontSize = Math.floor(fontScaleFactor * 100) + "%";
+		  padding = paddingIncrease + "% 0%";
+		  //padding = paddingIncrease + "% 0%";
+		  computedSide = computedSide + "px";
+		    
 		  
-		  $scope.secondarySkillStyle[i][j] = {'width': previousRemainderWidth, 'height': height, 'background-color': '#C2FFC2', 'margin':'5px 0px', 'text-align': 'center', 'vertical-align': 'middle' };
+		  //$scope.secondarySkillStyle[i][j] = {'width': previousRemainderWidth, 'height': height, 'background-color': '#C2FFC2', 'margin':'5px 0px', 'padding': padding, 'text-align': 'center', 'fontSize': fontSize };
+		  //$scope.secondarySkillStyle[i][j] = {'width': computedSide, 'height': computedSide, 'background-color': '#C2FFC2', 'margin':'5px 0px', 'padding': padding, 'text-align': 'center', 'fontSize': fontSize, 'word-break': wordBreak };
+		  $scope.secondarySkillStyle[i][j] = {'width': computedSide, 'height': computedSide, 'background-color': boxColor, 'margin':'5px 0px', 'text-align': 'center', 'fontSize': fontSize, 'word-break': wordBreak };
                 }
             }
 
