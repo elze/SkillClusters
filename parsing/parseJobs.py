5e5bff44f86557261a9755f1f30dffe8c25bf0de
Elze Hamilton <elzes@yahoo.com>
2015-09-25 00:30:36 -0500

    Fixed indentation, added synonyms, and many more skills

--- parsing/parseJobs.py
+++ parsing/parseJobs.py
@@ -43,23 +43,40 @@ from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 session = Session()
 
-skillKeywords = ["AJAX", "AMQP", "Android", "Angular", "Ant", "ApacheMQ", "Artifactory", "ASP.NET", "Backbone", "Bootstrap",
-                "C++", "C#", "Cassandra", "Chef", "Clojure", "CSS", "Crystal Reports", "Delphi", "DevOps", "Django",
-				"Eclipse", "Elm", "Ember", "Entity Framework", "Flash", "Flask", "FoxPro", "Haskell", "IIS",
-				"GIS", "Git", "Hadoop", "Handlebars", "Hibernate", "HTML", "HTML5", "J2EE", "Java", "Javascript",
-                "JBOSS", "JCE", "JDBC", "Jenkins", "JMS", "JNDI", "JPA", "JQuery", "JSON", "JUnit", 
-                "Labview", "LAMP", "Lua", "Linux", 
-				"Magento", "Maven", "Memcached", "MongoDB", "MVC", "MyBatis", "MySQL", 
-				".NET", "NHibernate", "Node", "Nose", "NoSQL", "NUnit",
-                "Oracle", "Perl", "Photoshop", "PHP", "PostGIS", "Postgres", "Python", "Puppet",
-				"RabbitMQ", "React", "REST", "Ruby", "Ruby on Rails", 
-				"Scala", "Selenium", "Silverlight", "SOAP", "Sparx", "Sphinx", "Spring",
-                "SQL", "SQL Server", "SSIS", "SSRS", "Struts", "Subversion", "SVN", "Symfony",
-				"Team Foundation Server", "TFS", "Teradata", "UNIX",
-				"VB.NET", "Visio", "Visual Basic", "Visual Studio",
-                "WCF", "Web Services", "WPF", "XML", "xUnit"]
+skillKeywords = ["AJAX", "Akka", "AMQP", "Android", "Angular", "AngularJS", "Ant", "ApacheMQ", "Artifactory", "ASP.NET",
+				 "Backbone", "BackboneJS", "Bootstrap", "BootstrapJS", "Bottle",
+				 "C++", "C#", "Cassandra", "Chef", "Clojure", "COBOL", "Cocoa", "CoffeeScript", "Cordova",
+                 "CouchDB", "Couch", "CSLA", "CSS", "CSS3", "Crystal Reports",
+				 "Dart", "D3", "D3JS", "Delphi", "DevOps", "Django", "Dynamo", "DynamoDB",
+				 "Eclipse", "Elixir", "Elm", "Ember", "EmberJS", "Entity Framework", "Erlang", "ES6",
+				 "Flash", "Flask", "Fortran", "FoxPro", "Haskell", "IIS",
+				 "GIS", "Git", "Golang", "Gradle", "Gulp",
+				 "Hadoop", "Handlebars", "HandlebarsJS", "HBase", "Hibernate", "Hive", "HTML", "HTML5",
+				 "IntelliJ", "iOS",
+				 "J2EE", "Java", "Javascript", "JBOSS", "JCE", "JDBC", "Jenkins", "JMS", "JNDI", "JPA", "JQuery", "JSON", "JUnit",
+				 "Kafka", "Kinesis", "Knockout", "KnockoutJS", "Koa", "KoaJS",
+				 "Labview", "LAMP", "Lua", "Linux",
+				 "Magento", "Maven", "Memcached", "Mercurial", "Mongo", "MongoDB", "MSSQL", "MS SQL", "MVC", "Mustache", "MyBatis", "MySQL",
+				 ".NET", "NHibernate", "Node", "NodeJS", "Nose", "NoSQL", "NUnit",
+				 "Objective C", "Objective-C", "Oracle",
+                 "Perl", "Phonegap", "Photoshop", "PHP", "PostGIS", "Postgres", "Python", "Puppet",
+				 "RabbitMQ", "RavenDB", "Raven", "RDBMS", "React.js", "ReactJS", "RESTful", "REST", "Ruby", "Ruby on Rails", "Rust",
+				 "Sass", "Scala", "Selenium", "Silverlight", "SOAP", "Sparx", "Sphinx", "Spring",
+				 "SQL", "SQL Server", "SSIS", "SSRS", "Struts", "Subversion", "SVN", "Symfony", "Swift",
+				 "Team Foundation Server", "TFS", "Telerik", "Teradata", "TransactSQL", "Transact SQL", "Transact-SQL", "TSQL", "T-SQL", "Typescript",
+                 "Underscore", "UnderscoreJS", "UML", "UNIX",
+				 "VB.NET", "Visio", "Visual Basic", "Visual Studio",
+				 "WCF", "WebAPI", "Web API", "Web Services", "WPF", "XML", "xUnit", "Zend"]
 
 
+
+synonymsDictionary = {'angularjs': 'angular', 'backbonejs': 'backbone', 'bootstrapjs': 'bootstrap', 'couchdb': 'couch',
+                      'd3js': 'd3', 'dynamodb': 'dynamo', 'emberjs': 'ember', 'handlebarsjs': 'handlebars', 'knockoutjs': 'knockout', 'koajs': 'koa',
+                      'mongodb': 'mongo', 'mssql': 'sql server', 'ms sql': 'sql server', 'nodejs': 'node', 'pbjective c': 'pbjective-c',
+                      'ravendb': 'raven', 'reactjs': 'react.js', 'restful': 'rest', 'svn': 'subversion',
+                      'tfs': 'team foundation server',
+                      'transactsql': 't-sql', 'transact sql': 't-sql', 'transact-sql': 't-sql', 'tsql': 't-sql',
+                      'underscorejs': 'underscore', 'web api': 'webapi' }
 
 skillsDictionary = {}
 
@@ -67,10 +84,6 @@ skillsPostCountsDict = {}
 
 matches = []
 
-
-
-#for jobFileName in listdir("c:\\Users\\elze\\Documents\\MyProjects\\Python\\EvernoteRetrieval\\Notes"):
-#    jobFile = open("c:\\Users\\elze\\Documents\\MyProjects\\Python\\EvernoteRetrieval\\Notes\\" + jobFileName, "r")
 for jobFileName in listdir(importFilePath):
     jobFile = open(importFilePath + jobFileName, "r")
     jobDescriptionRaw = jobFile.read().lower()
@@ -80,28 +93,35 @@ for jobFileName in listdir(importFilePath):
     #logging.debug(jobFileName)
     #logging.debug(jobDescription)
     #logging.debug("=====================")
-    for skillKeywordRaw in skillKeywords:
-	skillKeyword = skillKeywordRaw.lower()
-	#print skillKeyword + '\n'
-	m1 = re.search('\W(' +  re.escape(skillKeyword) + ')\W', jobDescription)
-	if m1:
-	    if skillKeyword in skillsDictionary:
-		secondarySkillDict = skillsDictionary[skillKeyword]
-		skillsPostCountsDict[skillKeyword] = skillsPostCountsDict[skillKeyword] + 1
-	    else:
-		secondarySkillDict = {}
-		skillsPostCountsDict[skillKeyword] = 1
-		
-	    for secondarySkillKeywordRaw in skillKeywords:
-		secondarySkillKeyword = secondarySkillKeywordRaw.lower()
-		if (skillKeyword != secondarySkillKeyword):
-		    m2 = re.search('\W(' + re.escape(secondarySkillKeyword) + ')\W', jobDescription)
-		    if m2:
-			if secondarySkillKeyword in secondarySkillDict:
-			    secondarySkillDict[secondarySkillKeyword] = secondarySkillDict[secondarySkillKeyword] + 1
-			else:
-			    secondarySkillDict[secondarySkillKeyword] = 1
-	    skillsDictionary[skillKeyword] = secondarySkillDict
+    for skillKeywordOrig in skillKeywords:
+        skillKeywordRaw = skillKeywordOrig.lower()
+        #print skillKeywordRaw + '\n'
+        skillKeyword = skillKeywordRaw
+        if skillKeywordRaw in synonymsDictionary:
+            skillKeyword = synonymsDictionary[skillKeywordRaw]
+        m1 = re.search('\W(' +  re.escape(skillKeyword) + ')\W', jobDescription)
+        if m1:
+            if skillKeyword in skillsDictionary:
+                secondarySkillDict = skillsDictionary[skillKeyword]
+                skillsPostCountsDict[skillKeyword] = skillsPostCountsDict[skillKeyword] + 1
+            else:
+                secondarySkillDict = {}
+                skillsPostCountsDict[skillKeyword] = 1
+
+            for secondarySkillKeywordOrig in skillKeywords:
+                secondarySkillKeywordRaw = secondarySkillKeywordOrig.lower()
+                secondarySkillKeyword = secondarySkillKeywordRaw
+                if secondarySkillKeywordRaw in synonymsDictionary:
+                    secondarySkillKeyword = synonymsDictionary[secondarySkillKeywordRaw]
+
+                if (skillKeyword != secondarySkillKeyword):
+                    m2 = re.search('\W(' + re.escape(secondarySkillKeyword) + ')\W', jobDescription)
+                    if m2:
+                        if secondarySkillKeyword in secondarySkillDict:
+                            secondarySkillDict[secondarySkillKeyword] = secondarySkillDict[secondarySkillKeyword] + 1
+                        else:
+                            secondarySkillDict[secondarySkillKeyword] = 1
+            skillsDictionary[skillKeyword] = secondarySkillDict
 
 for skillKeyword in skillsPostCountsDict:
     skillPostCounter = SkillPostCounter()
@@ -112,14 +132,13 @@ for skillKeyword in skillsPostCountsDict:
 for skillKeyword in skillsDictionary:
     secondarySkillHash = skillsDictionary[skillKeyword]
     for secondarySkillKeyword in secondarySkillHash:
-	secondarySkillCount = secondarySkillHash[secondarySkillKeyword]
-	skillpair = SkillPair()
-	skillpair.primary_term = skillKeyword
-	skillpair.secondary_term = secondarySkillKeyword
-	skillpair.number_of_times = secondarySkillCount
-	session.add(skillpair)
-
-	#logging.debug(pprint.pformat(skillpair.__dict__))
+        secondarySkillCount = secondarySkillHash[secondarySkillKeyword]
+        skillpair = SkillPair()
+        skillpair.primary_term = skillKeyword
+        skillpair.secondary_term = secondarySkillKeyword
+        skillpair.number_of_times = secondarySkillCount
+        session.add(skillpair)
+        #logging.debug(pprint.pformat(skillpair.__dict__))
 
 session.commit()
 
